14fc25857650d06845c6752a18417bacc8ceed3f
jorgemdnt <jorgemodes@gmail.com>
2016-09-11 14:04:57 -0300

    Adicionando instalacao do neovim

---
+++ install_neovim.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "-- Iniciar instalacao neovim --"
+if ! [ -x $(command -v nvim) ]; then
+    echo "-- Instalando neovim --"
+    echo `sudo apt-get install software-properties-common`
+    echo `sudo add-apt-repository ppa:neovim-ppa/unstable`
+    echo `sudo apt-get update`
+    echo `sudo apt-get install neovim`
+    echo `sudo apt-get install python-dev python-pip python3-dev python3-pip`
+else
+    echo "-- Neovim ja instalado --"
+fi
