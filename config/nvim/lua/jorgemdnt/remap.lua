7b9e1f9d9f786935a8252d48975b66f167c109c4
Jorge Modesto <7728049+jorgemdnt@users.noreply.github.com>
2023-10-07 16:43:55 -0300

    Added easier mappings

--- config/nvim/lua/jorgemdnt/remap.lua
+++ config/nvim/lua/jorgemdnt/remap.lua
@@ -4,7 +4,7 @@ vim.keymap.set("n", "<leader>w", vim.cmd.w)
 vim.keymap.set("n", "<leader>x", vim.cmd.x)
 vim.keymap.set("n", "<leader>q", vim.cmd.q)
 
-vim.keymap.set("n", "<leader>bd", vim.cmd.bdelete)
+-- vim.keymap.set("n", "<leader>bd", vim.cmd.bdelete)
 vim.keymap.set("n", "<C-J>", "<C-W><C-J>")
 vim.keymap.set("n", "<C-K>", "<C-W><C-K>")
 vim.keymap.set("n", "<C-K>", "<C-W><C-K>")
@@ -22,6 +22,7 @@ vim.keymap.set("v", "<leader>d", "\"_d")
 vim.keymap.set("n", "Q", "<nop>")
 
 vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+vim.keymap.set("v", "<leader>s", '"hy:%s/<C-r>h//gc<left><left><left>')
 
 vim.api.nvim_create_user_command(
     'TermCommand',
