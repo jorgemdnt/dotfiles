16acead8d5a9257fe7e2d0f71fab6e3d131a44db
Jorge Modesto <7728049+jorgemdnt@users.noreply.github.com>
2023-12-21 17:17:22 -0300

    Search current word in N and V modes using telescope

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -1,26 +1,32 @@
 local actions = require('telescope.actions')
 local builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<leader>f', builtin.find_files, {})
-vim.keymap.set("n", "<leader><space>", builtin.buffers, {})
--- vim.keymap.set('n', '<leader>fc', builtin.command_history, {})
+vim.keymap.set('n', '<leader><space>', builtin.buffers, {})
 vim.keymap.set('n', '<leader>m', builtin.marks, {})
-vim.keymap.set('n', '<leader>r', builtin.lsp_references, {})
 
 vim.keymap.set('n', '<leader>g', function()
-       builtin.grep_string({ search = vim.fn.input("Grep: ") })
+    builtin.grep_string({ search = vim.fn.input("Telescope grep: ") })
 end)
+vim.keymap.set('n', 'gw', function()
+    builtin.grep_string({ search = vim.fn.expand("<cword>") })
+end)
+vim.keymap.set('n', 'gW', function()
+    builtin.grep_string({ search = vim.fn.expand("<cWORD>") })
+end)
+vim.keymap.set('v', 'gw', '"hy:Telescope grep_string default_text=<C-r>h<CR>')
 
 local telescope = require('telescope')
 
 telescope.setup({
-  defaults = {
-    file_ignore_patterns = { "node_modules", ".git" },
-    mappings = {
-      i = {
-        ['<c-q>'] = actions.send_selected_to_qflist,
-      },
-    },
-  }
+    defaults = {
+        file_ignore_patterns = { "node_modules", ".git" },
+        mappings = {
+            i = {
+                ['<c-q>'] = actions.send_selected_to_qflist,
+            },
+        },
+    }
 })
 
 telescope.load_extension('fzf')
