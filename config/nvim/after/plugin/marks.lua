54394557aeaa6fdadff5d6fe2972d12f1e0a0d20
Jorge Modesto <7728049+jorgemdnt@users.noreply.github.com>
2023-12-28 11:59:58 -0300

    Remove default marks

--- config/nvim/after/plugin/marks.lua
+++ config/nvim/after/plugin/marks.lua
@@ -1,6 +1,6 @@
 require'marks'.setup {
   -- which builtin marks to show. default {}
-  builtin_marks = { ".", "<", ">", "^" },
+  builtin_marks = {},
   -- whether movements cycle back to the beginning/end of buffer. default true
   cyclic = true,
   -- whether the shada file is updated after modifying uppercase marks. default false
